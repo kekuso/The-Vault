76df2cbc5b0cef6e123a292cfa79a4298f6c4e6f
kekuso <kekuso808@gmail.com>
2016-05-18 07:00:16 -1000

    completed The-Vault

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function() {
-  
+  var secretKey = null;
+  return {
+    getValue: function(key) {
+      if(key) {
+        return secretKey;
+      }
+      else {
+        return null;
+      }
+    },
+
+    setValue: function(key, value) {
+      secretKey = value;
+    }
+  };
 };
